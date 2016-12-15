9ea0f6eb32249fa154cba6371aafcb97ddeb249c
Fischoff Nadine <NFischoff@schools.nyc.gov>
2016-12-15 14:19:37 -0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+
+
+    $( "button" ).click(function() {
+  $( "#menu-ep" ).slideToggle( 1000)});
+    
+    
+})(window);
